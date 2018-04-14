f1b064794fd320bbd222712d7a51af3c31582ae4
AhmadHaleem <coldman9910@gmail.com>
2018-04-14 19:15:11 +0200

    Make User Authentication

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'ahmad');
+    define('DB_PASS', '123123');
+    define('DB_NAME', 'postsMVC');
+
+    // APP Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+    // URL ROOT
+    define('URLROOT', 'http://php.com/sharePosts');
+    // Site name
+    define('SITENAME', 'SharePosts');
+    // App version
+    define('APPVERSION', '1.0.0');
---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+    class Pages extends Controller {
+        public function __construct()
+        {
+
+        }
+        public function index() {
+            $data = ['title' => 'SharePosts', 'description' => 'Simple Social Network built on the TraversyMVC php Framework'];
+            $this->view('pages/index', $data);
+        }
+
+        public function about() {
+            $data = ['title' => 'About', 'description' => 'Simple Social Network built on the TraversyMVC php Framework'];
+            $this->view('pages/about', $data);
+        }
+    }
